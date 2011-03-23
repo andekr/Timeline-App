f3ccbec9d7a9a97ad5939ed77d677b12b7def385
andekr <anderskri@gmail.com>
2011-03-23 17:36:32 +0100

    Image uri share fix

--- src/com/fabula/android/timeline/sync/GAEHandler.java
+++ src/com/fabula/android/timeline/sync/GAEHandler.java
@@ -108,7 +108,7 @@ public class GAEHandler {
 		    }else if(object instanceof Event){
 		    	for (EventItem eventI : ((Event)object).getEventItems()) {
 			    	if(eventI instanceof SimplePicture){
-			    		Uploader.uploadFile(Utilities.getRealPathFromURI(((SimplePicture)eventI).getPictureUri(), a), ((SimplePicture)eventI).getPictureFilename());
+			    		Uploader.uploadFile(Utilities.IMAGE_STORAGE_FILEPATH+((SimplePicture)eventI).getPictureFilename(), ((SimplePicture)eventI).getPictureFilename());
 			    	}
 				}
 		    }
