045a7cd3a7a7e95aa0f0f88091934512454ffd27
andrstor <andrstor87@gmail.com>
2011-03-30 14:06:36 +0200

    refactored the getAllEventsConnectedToTag methods

--- src/com/fabula/android/timeline/contentmanagers/TagManager.java
+++ src/com/fabula/android/timeline/contentmanagers/TagManager.java
@@ -149,19 +149,18 @@ public class TagManager {
 	
 	public ArrayList<BaseEvent> getAllEventsConnectedToTag(ArrayList <String> tagNames) {
 		
-		ArrayList<BaseEvent> allEventsConnectedToTag = new ArrayList<BaseEvent>();
+		ArrayList<BaseEvent> allEventsConnectedToTags = new ArrayList<BaseEvent>();
 		
 		for (String tagName : tagNames) {
 			for (BaseEvent baseEvent : getAllEventsConnectedToTag(tagName)) {
-				allEventsConnectedToTag.add(baseEvent);
+				allEventsConnectedToTags.add(baseEvent);
 			}
 		}
-		return allEventsConnectedToTag;
+		return allEventsConnectedToTags;
 	}
 	
 	public ArrayList<BaseEvent> getAllEventsConnectedToTag(String tagName) {
 		
-		ArrayList<String> allEventIDsConnectedToTag = getAllEventIDsConnectedToTag(tagName);
 		ArrayList<BaseEvent> allEventsConnectedToTag = new ArrayList<BaseEvent>();
 		ContentLoader contentLoader = new ContentLoader(context);
 		
@@ -172,10 +171,10 @@ public class TagManager {
 		for (Experience experience : allExperiencesInDatabase) {
 			new DatabaseHelper(context, experience.getTitle());
 			experience.setEvents(contentLoader.LoadAllEventsFromDatabase());
-			for (String eventID : allEventIDsConnectedToTag) {
-				 for (BaseEvent event : experience.getEvents()) {
-					if(event.getId().equals(eventID));
-					allEventsConnectedToTag.add(event);
+			
+			for (BaseEvent baseEvent : experience.getEvents()) {
+				if(baseEvent.hasTag(tagName)) {
+					allEventsConnectedToTag.add(baseEvent);
 				}
 			}
 		DatabaseHelper.getCurrentTimelineDatabase().close();
